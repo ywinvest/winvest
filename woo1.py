e55384e3b13f6ca4e8356e07e1eb11d58cf2472a
우창호 <changhwoo@gmarket.com>
2025-06-18 12:25:23 +0900

    feat(woo1): adjust buy condition

--- woo1.py
+++ woo1.py
@@ -42,7 +42,7 @@ def filter_common_stocks(df):
 def buy_condition(df):
   # 벡터화된 연산 사용
   conditions = pd.Series(True, index=df.index)
-  conditions &= (df['Close'] <= df['52WeekLow'] * 1.3)
+  # conditions &= (df['Close'] <= df['52WeekLow'] * 1.3)
   conditions &= (df['Change'] >= 0)
   conditions &= (df['High_Change'] >= 8)
   conditions &= (df['Bullish'])
@@ -52,7 +52,7 @@ def buy_condition(df):
   # conditions &= ~((df['Pre_Volume_Change'] > 3) & (df['Pre_Change'] > 0)) # 전봉 거래량 300% 초과 + 등락률 0% 초과 제외
   conditions &= (df['Pre_Volume_Change'] <= 4) # 전봉 거래량 400% 이하
   conditions &= (df['Close'] >= df['MA20'])
-  # conditions &= (df['Low'] != df['52WeekLow']) # 52주 신저가 경신 제외
+  conditions &= (df['Low'] != df['52WeekLow']) # 52주 신저가 경신 제외
   return conditions
 
 def create_rich_text_with_imoji(emoji, text, bold=True):
