cbd2125d2b20e9fed98add1c17caf22670e7f244
우창호 <changhwoo@gmarket.com>
2025-06-30 08:39:19 +0900

    feat(woo4): send signal message to slack

---
+++ woo4.py
@@ -0,0 +1,308 @@
+import concurrent.futures
+import os
+import time
+from datetime import datetime
+from functools import partial
+
+import FinanceDataReader as fdr
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+SLACK_API_URL = "https://slack.com/api"
+
+def calculate_indicators(df):
+  # df['MA5'] = df['Close'].rolling(window=5).mean()
+  # df['MA10'] = df['Close'].rolling(window=10).mean()
+  df['MA20'] = df['Close'].rolling(window=20).mean()
+  df['MA60'] = df['Close'].rolling(window=60).mean()
+  df['MA120'] = df['Close'].rolling(window=120).mean()
+  df['MA20_Cross'] = (df['Close'].gt(df['MA20'], axis=0)) & (df['Close'].shift(1).le(df['MA20'].shift(1), axis=0))
+  df['MA20_Break'] = (df['Close'].lt(df['MA20'], axis=0)) & (df['Close'].shift(1).ge(df['MA20'].shift(1), axis=0))
+  df['Bullish'] = df['Close'] > df['Open']
+  df['Volume_Change'] = df['Volume'] / df['Volume'].shift(1) - 1
+  # df['High_Change'] = (df['High'] / df['Close'].shift(1) - 1) * 100
+  # df['Pre_Change'] = df['Change'].shift(1)
+  # # df['Pre_Bullish'] = df['Close'].shift(1) > df['Open'].shift(1)
+  # df['Pre_High_Change'] = df['High_Change'].shift(1)
+  # df['Pre_Volume_Change'] = df['Volume'].shift(1) / df['Volume'].shift(2)
+  # df['Crossover'] = (df['MA5'] > df['MA20']) & (df['MA5'].shift(1) <= df['MA20'].shift(1))
+  # df['Crossover_Count'] = df['Crossover'].rolling(window=30, min_periods=1).sum()
+  df['Pre39WeekHigh'] = df['High'].shift(1).rolling(window='273D', min_periods=1).max()
+
+  df['Pre52WeekHigh'] = df['High'].shift(1).rolling(window='364D', min_periods=1).max()
+
+  # 39주 신고가 돌파 여부
+  is_39weekhigh_break = df['Close'] > df['Pre39WeekHigh']
+  # 52주 신고가 돌파 여부
+  is_52weekhigh_break = df['Close'] > df['Pre52WeekHigh']
+
+  # 연속적인 신고가 돌파를 그룹화하여 첫 돌파만 선택
+  # 돌파가 시작되는 지점을 그룹화 기준으로 사용
+  # breaks = (~is_52weekhigh_break).cumsum()  # 돌파가 끊기는 지점으로 그룹화
+  # is_first_break = is_52weekhigh_break & (~is_52weekhigh_break.shift(1).fillna(False))
+  # df['First_52WeekHigh_Break'] = is_first_break.groupby(breaks).cumsum() == 1
+  # df['First_52WeekHigh_Break'] = df['First_52WeekHigh_Break'].where(is_52weekhigh_break, False)
+  # 39주 신고가 첫 돌파여부
+  df['First_39WeekHigh_Break'] = is_39weekhigh_break & (~is_39weekhigh_break.shift(1, fill_value=False))
+  # 52주 신고가 첫 돌파여부
+  df['First_52WeekHigh_Break'] = is_52weekhigh_break & (~is_52weekhigh_break.shift(1, fill_value=False))
+  # 첫 돌파 이후 10일 동안 추가 돌파 무시
+  # df['First_52WeekHigh_Break'] = df['First_52WeekHigh_Break'].where(
+  #     ~df['First_52WeekHigh_Break'].rolling(window=10, min_periods=1).sum().shift(1).fillna(0).astype(bool),
+  #     False
+  # )
+
+  # 이동평균선 추세 상승 여부 (기울기 > 0)
+  # df['MA20_Uptrend'] = df['MA20'] > df['MA20'].shift(1)
+  # df['MA60_Uptrend'] = df['MA60'] > df['MA60'].shift(1)
+  # df['MA120_Uptrend'] = df['MA120'] > df['MA120'].shift(1)
+  df['MA20_Slope'] = df['MA20'].pct_change(fill_method=None)
+  df['MA60_Slope'] = df['MA60'].pct_change(fill_method=None)
+  df['MA120_Slope'] = df['MA120'].pct_change(fill_method=None)
+
+  # 벡터화된 연속 상승 일수 계산
+  def calculate_uptrend_days_vec(uptrend_series):
+    """벡터화 방식으로 연속 상승 일수를 계산"""
+    # 상승 추세가 끊기는 지점을 그룹화 기준으로 사용
+    breaks = (~uptrend_series).cumsum()
+    # 각 그룹 내에서 연속된 True의 개수 계산
+    uptrend_days = uptrend_series.groupby(breaks).cumsum()
+    # 상승 추세가 아닌 경우(False)는 0으로 설정
+    uptrend_days = uptrend_days.where(uptrend_series, 0)
+    return uptrend_days
+
+  # 각 MA에 대해 추세 상승 유지 일수 추가
+  df['MA20_Uptrend_Days'] = calculate_uptrend_days_vec(df['MA20_Slope'] > 0)
+  df['MA60_Uptrend_Days'] = calculate_uptrend_days_vec(df['MA60_Slope'] > 0)
+  df['MA120_Uptrend_Days'] = calculate_uptrend_days_vec(df['MA120_Slope'] > 0)
+
+  df['MA20_Gap'] = df['Close'] / df['MA20'] - 1
+  return df
+
+def filter_common_stocks(df):
+  # ETN, ETF, 리츠, 선박펀드, 스팩 제외
+  exclude_pattern = r'ETN|ETF|리츠|선박펀드|스팩'
+  return df[(~df['Name'].str.contains(exclude_pattern, na=False, regex=True))
+            & (~df['Code'].str.endswith("5", na=False)) # 우선주
+            & (~df['Code'].str.endswith("7", na=False)) # 우선주
+            & (~df['Code'].str.endswith("9", na=False)) # 우선주
+            & (~df['Code'].str.endswith("K", na=False)) # 우선주
+            & (~df['Code'].str.endswith("L", na=False)) # 우선주
+            # & (df['Name'].str.contains("나무기술", na=False, regex=True))
+            ]
+
+def buy_condition(df, market):
+  # 벡터화된 연산 사용
+  conditions = pd.Series(True, index=df.index)
+  # conditions &= (df['MA60_Uptrend'])
+  # conditions &= (df['MA120_Uptrend'])
+  # conditions &= (df['MA20_Cross'])
+  # conditions &= (df['Close'] > df['Pre52WeekHigh'])
+  if market == 'KOSPI' or market == 'KOSDAQ GLOBAL':
+    conditions &= (df['Pre52WeekHigh'] != 0)
+    conditions &= (df['First_52WeekHigh_Break'])
+  elif market == 'KOSDAQ':
+    conditions &= (df['Pre39WeekHigh'] != 0)
+    conditions &= (df['First_39WeekHigh_Break'])
+  # conditions &= (df['MA20_Uptrend'] == True)
+  # conditions &= (df['MA60_Uptrend'] == True)
+  # conditions &= (df['MA120_Uptrend'] == True)
+  conditions &= (df['MA20_Slope'] > 0)
+  conditions &= (df['MA60_Slope'] > 0)
+  conditions &= (df['MA120_Slope'] > 0)
+  conditions &= (df['Change'] < 0.295)
+  conditions &= (df['Volume'] > 0)
+  conditions &= (df['Volume'].shift(1) > 0)
+  conditions &= (df['MA120_Uptrend_Days'] < 400) # 120일 상승 추세 장기 연속 제외
+  conditions &= ((df['Close'] - df['Open'])/df['Close'] > -0.05) # 긴 음봉 제외
+  # conditions &= (df['MA20_Gap'] < 0.3)
+  return conditions
+
+def create_rich_text_with_imoji(emoji, text, bold=True):
+  return {
+    "type": "rich_text_section",
+    "elements": [
+      {
+        "type": "emoji",
+        "name": emoji,
+      },
+      {
+        "type": "text",
+        "text": text,
+        "style": {
+          "bold": bold
+        }
+      }
+    ]
+  }
+
+def create_rich_text_item(text, bold=False):
+  return {
+    "type": "rich_text_section",
+    "elements": [
+      {
+        "type": "text",
+        "text": text,
+        "style": {
+          "bold": bold
+        }
+      }
+    ]
+  }
+
+def send_slack_message(blocks):
+  # 설정 로드
+  # with open("config-woo1.json", "r") as config_file:
+  #   config = json.load(config_file)
+
+  # slack_token = config["slack_bot_token"]
+  # slack_channel = config["slack_channel"]
+  bot_token = os.getenv("SLACK_BOT_TOKEN")
+  channel = os.getenv("SLACK_CHANNEL")
+
+  headers = {
+    "Authorization": f"Bearer {bot_token}",
+    "Content-Type": "application/json"
+  }
+
+  payload = {
+    "channel": channel,
+    "blocks": blocks
+  }
+
+  response = requests.post(f"{SLACK_API_URL}/chat.postMessage", headers=headers, json=payload)
+  response_data = response.json()
+  if not response_data.get("ok"):
+    raise Exception(f"Failed to send message: {response_data.get('error')}")
+  return response_data.get("ts")
+
+def format_market_cap(marcap):
+  """시가총액을 조 또는 억 단위로 포맷팅"""
+  if marcap >= 1e12:  # 1조 이상
+    return f"{marcap/1e12:.1f}조"
+  else:  # 억 단위
+    return f"{marcap/1e8:.0f}억"
+
+def send_to_slack(result_data):
+  try:
+    if result_data.empty:
+      blocks = [
+        {
+          "type": "section",
+          "text": {
+            "type": "plain_text",
+            "text": "오늘은 매수 후보가 없습니다.",
+            "emoji": True
+          }
+        }
+      ]
+      send_slack_message(blocks)
+      print("No stocks match the buying conditions")
+      return
+
+    # 결과를 Market과 High_Change를 기준으로 정렬
+    # result_data = result_data.sort_values(['Market', 'High_Change'], ascending=[True, False])
+
+    # 시장별로 데이터 구성
+    rich_text_elements = []
+    rich_text_elements.append(create_rich_text_item(
+        f" {today.year}년 {today.month}월 {today.day}일 매수 후보\n", True
+    ))
+    for market, group in result_data.groupby('Market'):
+      for _, row in group.iterrows():
+        name = row['Name']
+        marcap = row['Marcap']
+        open = row['Open']
+        close = row['Close']
+        high = row['High']
+
+        message = f"{name} : {format_market_cap(marcap)}"
+
+        # Rich Text 아이템 생성
+        item = create_rich_text_with_imoji(
+            "question", message, False
+        )
+
+        rich_text_elements.append(item)
+
+    blocks = [
+      {
+        "type": "rich_text",
+        "elements": rich_text_elements
+      }
+    ]
+
+    # Slack 메시지 전송
+    send_slack_message(blocks)
+
+  except Exception as e:
+    print(f"Error sending Slack message: {e}")
+
+def process_stock(row, two_years_ago, today):
+  try:
+    ticker = row['Code']
+    name = row['Name']
+    marcap = row['Marcap']
+    market = row['Market']
+
+    df = fdr.DataReader(ticker, two_years_ago)
+    df = calculate_indicators(df)
+
+    buys = df[buy_condition(df, market)]
+    buys = buys[buys.index.date == today.date()]
+
+    if not buys.empty:
+      buys['Ticker'] = ticker
+      buys['Name'] = name
+      buys['Marcap'] = marcap
+      buys['Market'] = market
+      return buys
+    return None
+  except Exception as e:
+    print(f"Error processing {ticker}: {e}")
+    return None
+
+def parallel_process_stocks(all_stocks, two_years_ago, today):
+  process_func = partial(process_stock, two_years_ago=two_years_ago, today=today)
+  results = []
+
+  with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    futures = [executor.submit(process_func, row) for _, row in all_stocks.iterrows()]
+    for future in concurrent.futures.as_completed(futures):
+      result = future.result()
+      if result is not None:
+        results.append(result)
+
+  return pd.concat(results) if results else pd.DataFrame()
+
+if __name__ == "__main__":
+  start_time = time.time()
+
+  # .env 파일 로드
+  load_dotenv()
+
+  try:
+    # 종목 리스트 가져오기 및 필터링
+    all_stocks = pd.concat([
+      filter_common_stocks(fdr.StockListing('KOSPI')),
+      filter_common_stocks(fdr.StockListing('KOSDAQ'))
+    ], ignore_index=True)
+
+    # 날짜 설정
+    today = datetime.today()
+    two_years_ago = today.year - 2
+
+    # 병렬 처리로 데이터 분석
+    result_data = parallel_process_stocks(all_stocks, two_years_ago, today)
+
+    # Slack 메시지 전송
+    send_to_slack(result_data)
+
+  except Exception as e:
+    print(f"Error in main execution: {e}")
+
+  finally:
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"총 소요시간: {elapsed_time:.2f}초")
