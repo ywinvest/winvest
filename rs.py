c100be2ec17a756577b4f2e21b5df27f3b7cf1cd
우창호 <changhwoo@gmarket.com>
2025-07-11 09:54:31 +0900

    test: rs

--- rs.py
+++ rs.py
@@ -47,9 +47,11 @@ def process_stock(row, two_years_ago):
   """개별 종목의 시세 데이터를 가져와 보조지표를 계산합니다."""
   try:
     symbol = row['Code']
+    market = row['Market']
 
     df = fdr.DataReader(symbol, two_years_ago)
     df = calculate_indicators(df)
+    df['Market'] = market
 
     return df
   except Exception as e:
