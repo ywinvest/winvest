cb585b1e4cd105c3f283642dbc9948460d67a1fb
우창호 <changhwoo@gmarket.com>
2025-07-14 16:32:52 +0900

    test: rs

--- rs.py
+++ rs.py
@@ -10,17 +10,17 @@ from dotenv import load_dotenv
 
 def calculate_indicators(df):
   first_day_close = df['Close'].iloc[0]
-  for period_days in [21, 63, 126, 252]:
+  for period_days in [21, 63, 126, 189, 252]:
     period_str = f"{period_days // 21}M"# if period_days < 252 else "12M"
     return_col = f'Return_{period_str}'
     base_price = df['Close'].shift(period_days)
     base_price.fillna(first_day_close, inplace=True)
     df[return_col] = df['Close'] / base_price - 1
 
-  df['Weighted_Return'] = (df['Return_1M'] * 0.4 +
-                           df['Return_3M'] * 0.3 +
-                           df['Return_6M'] * 0.2 +
-                           df['Return_12M'] * 0.1)
+  df['Weighted_Return'] = (df['Return_3M'] * 2 +
+                           df['Return_6M'] +
+                           df['Return_9M'] +
+                           df['Return_12M'])
   return df
 
 
@@ -44,7 +44,7 @@ def calculate_relative_strength(df):
     return scaled_rs
 
   # Apply the scaling function to each return period
-  for period in ["1M", "3M", "6M", "12M"]:
+  for period in ["1M", "3M", "6M", "9M", "12M"]:
     return_col = f'Return_{period}'
     rs_col = f'RS_{period}'
     df[rs_col] = grouped[return_col].transform(scale_rank)
@@ -53,7 +53,7 @@ def calculate_relative_strength(df):
   df['RS'] = grouped['Weighted_Return'].transform(scale_rank)
 
   # Round, fill any remaining NaNs, convert to integer, and clip to the 1-99 range
-  rs_cols = [f'RS_{p}' for p in ["1M", "3M", "6M", "12M"]] + ['RS']
+  rs_cols = [f'RS_{p}' for p in ["1M", "3M", "6M", "9M", "12M"]] + ['RS']
   for col in rs_cols:
     df[col] = df[col].round().fillna(1).astype(int).clip(1, 99)
 
