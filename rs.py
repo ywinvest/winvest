5b0fff1646783e9ef7d9ba7680879c1002471224
우창호 <changhwoo@gmarket.com>
2025-07-18 12:24:55 +0900

    test: rs

--- rs.py
+++ rs.py
@@ -133,8 +133,11 @@ if __name__ == "__main__":
     ], ignore_index=True)
 
     all_stocks = all_stocks[
-      (all_stocks['Name'] != "0030R0") &
-      (all_stocks['Name'] != "0030T0")
+      (all_stocks['Code'] != "0030R0") &
+      (all_stocks['Code'] != "0030T0") &
+      (all_stocks['Name'] != "싸이닉솔루션") &
+      (all_stocks['Code'] != "아우토크립트") &
+      (all_stocks['Code'] != "뉴엔AI")
       ]
     url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
     headers = {
