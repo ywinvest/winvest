cd6c790dd5b3bfd7486a59ffb716246441a1c44d
우창호 <changhwoo@gmarket.com>
2025-12-24 12:44:00 +0900

    feat(woo2): sell on volume spike drop for partial sell

--- woo2.py
+++ woo2.py
@@ -154,8 +154,8 @@ def sell_on_volume_spike_drop(df):
   return (
       (df['Volume'] > df['Volume'].shift(1) * 1.2) &
       (df['Change'] < -1 * BASE_RISK) &  # -8%
-      (df['Close'] / df['Open'] - 1 < -1 * BASE_RISK) & # -8%
-      ((df['High'] - df[['Open', 'Close']].max(axis=1)) / df['Open'] > 0.01)
+      (df['Close'] / df['Open'] - 1 < -1 * BASE_RISK) # -8%
+      # ((df['High'] - df[['Open', 'Close']].max(axis=1)) / df['Open'] > 0.01)
   )
 
 def buy_and_sell(df, kospi_df, kosdaq_df):
