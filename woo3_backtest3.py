7b11bce3d365918ac09ec8f65a189b36311a1a25
우창호 <changhwoo@gmarket.com>
2026-01-02 14:43:46 +0900

    feat(woo3): add woo3 backtest

--- woo3_backtest3.py
+++ woo3_backtest3.py
@@ -264,7 +264,7 @@ def strategy_nb(c, data, buy_count_state, last_price_state, max_positions, init_
           # fees=0.
       )
 
-  return vbt_nb.no_order_func_nb()
+  return vbt_nb.order_nothing_nb()
 
   # return pf_enums.OrderResult(
   #     size=np.nan,
