0bae362d283b252efaf7c30d5880f9a90fba9452
우창호 <changhwoo@gmarket.com>
2026-04-06 09:21:07 +0900

    feat(woo4): short term trend following

--- woo4.py
+++ woo4.py
@@ -138,7 +138,7 @@ def buy_condition(df):
   #     ((kospi_or_kosdaq_global) & df['Pre52WeekHigh'].ne(0) & df['First_52WeekHigh_Break']) |
   #     ((kosdaq) & df['Pre39WeekHigh'].ne(0) & df['First_39WeekHigh_Break'])
   # )
-  conditions &= (df['Pre52WeekHigh'].ne(0) & df['First_52WeekHigh_Break'])
+  # conditions &= (df['Pre52WeekHigh'].ne(0) & df['First_52WeekHigh_Break'])
   # conditions &= (df['MA20_Uptrend'] == True)
   # conditions &= (df['MA60_Uptrend'] == True)
   # conditions &= (df['MA120_Uptrend'] == True)
